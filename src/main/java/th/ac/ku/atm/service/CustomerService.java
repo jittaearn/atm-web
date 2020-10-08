3d320482adf3826470e43b4da0f7d8ad7d191ea5
jittaearn <jitta.k@ku.th>
2020-10-08 10:54:40 +0700

    customer web service API

--- src/main/java/th/ac/ku/atm/service/CustomerService.java
+++ src/main/java/th/ac/ku/atm/service/CustomerService.java
@@ -1,46 +1,41 @@
 package th.ac.ku.atm.service;
 
 import org.mindrot.jbcrypt.BCrypt;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
+import th.ac.ku.atm.data.CustomerRepository;
 import th.ac.ku.atm.model.Customer;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.NoSuchElementException;
 
 @Service
 public class CustomerService {
+    private CustomerRepository repository;
 
-    private ArrayList<Customer> customerList = new ArrayList<>();
+    public CustomerService(CustomerRepository repository) {
+        this.repository = repository;
+    }
 
     public void createCustomer(Customer customer) {
         String hashPin = hash(customer.getPin());
         customer.setPin(hashPin);
-        customerList.add(customer);
+        repository.save(customer);
     }
 
     public List<Customer> getCustomers() {
-        return new ArrayList<>(customerList);
+        return repository.findAll();
     }
 
-    public Customer findCustomer(int id) {
-        for (Customer customer : customerList) {
-            if (customer.getId() == id)
-                return customer;
-        }
-        return null;
-    }
     public Customer checkPin(Customer inputCustomer) {
-        // 1. หา customer ที่มี id ตรงกับพารามิเตอร์
         Customer storedCustomer = findCustomer(inputCustomer.getId());
 
-        // 2. ถ้ามี id ตรง ให้เช็ค pin ว่าตรงกันไหม โดยใช้ฟังก์ชันเกี่ยวกับ hash
         if (storedCustomer != null) {
-            String hashPin = storedCustomer.getPin();
-
-            if (BCrypt.checkpw(inputCustomer.getPin(), hashPin))
+            String storedPin = storedCustomer.getPin();
+            if (BCrypt.checkpw(inputCustomer.getPin(), storedPin))
                 return storedCustomer;
         }
-        // 3. ถ้าไม่ตรง ต้องคืนค่า null
         return null;
     }
 
@@ -48,6 +43,13 @@ public class CustomerService {
         String salt = BCrypt.gensalt(12);
         return BCrypt.hashpw(pin, salt);
     }
+
+    public Customer findCustomer(int id) {
+        try {
+            return repository.findById(id).get();
+        } catch (NoSuchElementException e) {
+            return null;
+        }
+    }
+
 }
-
-
